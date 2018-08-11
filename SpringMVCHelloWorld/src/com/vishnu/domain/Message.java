a2ee64bd0607152199e41eb0f0e95be7fe1db548
Vishnu <vishnu8901@gmail.com>
2018-08-11 13:05:55 +0530

    domain package and class

---
+++ SpringMVCHelloWorld/src/com/vishnu/domain/Message.java
@@ -0,0 +1,31 @@
+package com.vishnu.domain;
+
+import java.util.Date;
+
+public class Message {
+	
+	String name;
+	String message;
+	Date date;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+
+}
